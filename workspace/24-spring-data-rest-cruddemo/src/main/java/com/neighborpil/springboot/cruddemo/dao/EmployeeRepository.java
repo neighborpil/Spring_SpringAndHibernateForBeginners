e8160371fad0edcb3bb0af927e4bf544d250b5af
Neighborpil <neighborpil@naver.com>
2021-09-03 08:45:20 +0900

    save

--- workspace/24-spring-data-rest-cruddemo/src/main/java/com/neighborpil/springboot/cruddemo/dao/EmployeeRepository.java
+++ workspace/24-spring-data-rest-cruddemo/src/main/java/com/neighborpil/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.neighborpil.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.neighborpil.springboot.cruddemo.entity.Employee;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	// no need to write any code at all
